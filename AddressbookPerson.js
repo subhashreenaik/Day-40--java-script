897ad1e0bcf66cbc3981623109afa27b1804dbb4
Subhashree naik <subhashree.naik@gmail.com>
2022-04-07 19:37:48 +0530

    Ability to check the regex pattern

--- AddressbookPerson.js
+++ AddressbookPerson.js
@@ -9,6 +9,7 @@ class Person {
     address;
     city;
     state;
+    
 
     //Constructor
     constructor(firstName, lastName, emailId, phoneNumber, zipCode, address, city, state) {
@@ -47,3 +48,27 @@ class Person {
         return this.state;
     }
 }
+
+//UC 2:-
+    /**
+ *  Regex patterns
+ */
+/**
+ * @purpose -> UC2 : Ability to ensure Valid Contacts are added...Contacts are added... 
+ *          -  First Name and Last Name should start with Capital 
+ *          -  Minimum 3 Characters Address, City and State should also have minimum 4 characters
+ *          -  Zip, Phone and Email should be valid 
+ *          -     
+ * **/
+
+     const pattern_NAME = /^[A-Z]{1}[a-z]{2,}$/;
+     const pattern_EMAILID = /^[A-Za-z0-9.+-]*[a-zA-Z0-9]+[@][a-z0-9]+[.][a-z]+[.]?[a-z]+/;
+     const pattern_PHONENUMBER = /(91\s)[5-9][0-9]{9}/;
+     const pattern_ZipCode = /^[0-9]{5,10}/;
+     const pattern_ADDRESS_CITY_STATE = /^[A-Za-z0-9.+,-\s]{4,}$/;
+
+
+
+
+
+
